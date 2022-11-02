39a96ac8d41c0c3dde9eb682c4ff64d99cf2f3ad
RealRude <jerryvohanka@gmail.com>
2022-11-02 14:45:34 +0100

    Upgraded lowering and raising ranges on subsequent computer guesses.

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ user_number = int(input("Please pick a number between 1 and 10: "))
 
 print("The user number is: ", user_number)
 
-computer_guess = random.randrange(1, 11)
+lower_range = 1
+upper_range = 11
+computer_guess = random.randrange(lower_range, upper_range)
 print("First computer guess is: " + str(computer_guess))
 if computer_guess == user_number:
     print("Computer managed the right guess on his first try!")
@@ -18,19 +20,29 @@ else:
     while user_number != computer_guess:
         if computer_guess > user_number:
 
-            print("Computer guess:" + str(computer_guess))
-            show_range = computer_guess - 1
-            print(f"The computer guess ({computer_guess}) was too high. Now guessing in range from 1 to " + str(show_range))
-            computer_guess = random.randrange(1, computer_guess)
+            print("Computer guess: " + str(computer_guess))
+
+            #lowers the upper_range appropriately for another guess cycle
+            upper_range = upper_range - (upper_range - computer_guess)
+
+            print(f"The computer guess ({computer_guess}) was too high. Upper range is now: {upper_range}. Now guessing in range from {lower_range} to {upper_range}")
+
+            #let's the computer guess again
+            computer_guess = random.randrange(lower_range, upper_range)
 
         elif computer_guess < user_number:
 
-            print("Computer guess:" + str(computer_guess))
-            show_range = computer_guess + 1
-            print(f"The computer guess ({computer_guess}) was too low. Guessing in range from " + str(show_range) + " to 11")
-            computer_guess = random.randrange(computer_guess, 11)
+            print("Computer guess: " + str(computer_guess))
 
-print("Computer guess:" + str(computer_guess))
+            #raises the lower_range appropriately for another guess cycle
+            lower_range = computer_guess + 1
+
+            print(f"The computer guess ({computer_guess}) was too low. Lower range is now: {lower_range}. Guessing in range from {lower_range} to {upper_range}")
+
+            #let's the computer guess again
+            computer_guess = random.randrange(lower_range, upper_range)
+
+print("Computer guess: " + str(computer_guess))
 print("Computer finally guessed the right number which was: " + str(computer_guess))
 
 endprint = "\n\n>>>|END OF PROGRAM|<<<"
